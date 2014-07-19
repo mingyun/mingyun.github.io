e88676332ff9ce1c0368f58dfca951c11bf3a725
lovecn <mybestpartner@sina.com>
2014-07-19 11:43:31 +0800

    love story

---
+++ love2/js/console.js
@@ -0,0 +1,7 @@
+if (window.console) {
+    var cons = console;
+    if (cons) {
+        cons.log('在这个日子里，我只想对你、对你们说：今生能遇见你们真的很好。\n也许每个人都有每个人的苦，每个人都有每个人的泪，但即便是擦肩而过的美好，也值得我们深深的记住。\n谢谢你们。\n过去，现在，将来，如果遇到生命中对的那个人，请一定要好好把握呢！');
+        cons.log("Copyright 2013  %c❤", "color:rgb(246,105,147);font-weight:bold;");
+    }
+}
